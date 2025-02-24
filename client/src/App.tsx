24b4f6820242375ef359ab6a9fd766061fc42886
kaushiktak19 <kaushiktak19@gmail.com>
2025-02-24 19:06:35 +0530

    inital setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="text-3xl">Click me</Button>
+    </div>
+  )
+}
+
