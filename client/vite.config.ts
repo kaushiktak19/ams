2ca1e3bcde47a936c5c0bbb6771479a9dcaa4685
kaushiktak19 <kaushiktak19@gmail.com>
2025-02-26 20:36:31 +0530

    Deployment fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,11 +2,13 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const backendUrl = process.env.BACKEND_URL ?? '';
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000https://apartments-0h6y.onrender.com'
+      '/api': backendUrl,
     }
   },
   resolve: {
